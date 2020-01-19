efb2766f49480f6a5f1a65477b874a073c45b342
peterboldyrev <53663056+peterboldyrev@users.noreply.github.com>
2020-01-18 20:15:14 -0800

    updates from 01/18

--- 20200118-soccerApi.js
+++ 20200118-soccerApi.js
@@ -1,15 +1,53 @@
+var teamName = prompt("Choose the team");
+var id;
+
 $.ajax({
 headers:{'X-Auth-Token' :'3d5af28fa6024c74b54c5588d1536b09'},
-url: 'http://api.football-data.org/v2/teams/66/matches/' ,
+url: 'http://api.football-data.org/v2/competitions/2016/teams' ,
 dataType: 'json',
 type: 'GET'
 }).then(function(response){
+
   console.log(response);
+$("#scoreDiv").text(response);
+
+for(var i=0; i<response.teams.length;i++){
+  
+  if(response.teams[i].name === teamName){
+    id = response.teams[i].id;
+  }
+  console.log(id);
+  console.log(teamName);
+}
+
+getMatches();
+
 });
 
 
 
+function getMatches(){
+                          $.ajax({
+                            headers:{'X-Auth-Token' :'3d5af28fa6024c74b54c5588d1536b09'},
+                            url: `http://api.football-data.org//v2/teams/${id}/matches/` ,
+                            dataType: 'json',
+                            type: 'GET'
+                            }).then(function(response){
 
+                        console.log(response);
+
+                            });
+};
+
+// console.log(response.matches[5].status);
+
+// competition id 2016
+
+
+// /v2/competitions/2084/standings
+// /v2/teams/{id}/matches/
+
+// /v2/competitions/{id}/teams
 
 // 'http://api.football-data.org/v2/teams/{id}';
 
